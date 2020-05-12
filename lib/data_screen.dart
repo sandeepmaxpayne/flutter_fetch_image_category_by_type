03c815103fa1c7dc22e214b035fa302c5e1091d2
DESKTOP-D7NMFO9\Sandeep <www.sandeepbabu45gmail.com>
2020-05-12 18:42:16 +0530

    Finalize with toggle button

--- lib/data_screen.dart
+++ lib/data_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_fetcch_api/category_data.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class DataScreen extends StatefulWidget {
   @override
@@ -12,8 +14,8 @@ class _DataScreenState extends State<DataScreen> {
   String name;
   String img;
   List<Map<String, String>> dataList = [];
-  var petData;
-  String type = "pets";
+
+  final String type = "pets";
 
   @override
   void initState() {
@@ -24,18 +26,34 @@ class _DataScreenState extends State<DataScreen> {
   void getapiData() async {
     var categoryData = await CategoryData().getpetData();
     print("category data: $categoryData");
-    petData = categoryData;
-    if (type == "pets") {
-      updateData(petData);
-    }
+    var petData = categoryData;
+    updateData(petData);
+  }
+
+  void getapinatureData() async {
+    var categoryData = await CategoryData().getnatureData();
+    print("category data: $categoryData");
+    var natureData = categoryData;
+    updateData(natureData);
+  }
+
+  void toggle2() {
+    getapinatureData();
+    //return Container();
+  }
+
+  void toggle1() {
+    getapiData();
+    // return Container();
   }
 
   void updateData(dynamic data) {
+    dataList.clear();
     setState(() {
       for (int i = 0; i <= 9; i++) {
         name = data[i]['user']['name'];
         img = data[i]['urls']['raw'];
-        print("name: $name");
+        print("$i. name: $name");
         dataList.add({"name": name, "img": img});
       }
       // print("data:     $dataList");
@@ -45,81 +63,96 @@ class _DataScreenState extends State<DataScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final _screenSize = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.yellow,
-          title: Text(
-            'Pet and Nature',
-            style: TextStyle(
-              color: Colors.white,
-            ),
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        title: Text(
+          'Pet and Nature',
+          style: TextStyle(
+            color: Colors.white,
           ),
-          centerTitle: true,
         ),
-        body: Column(
-            children: <Widget>[
-              Row(children: <Widget>[
-                Text("Pets")
-              ]),
-              dataList.length <= 7
-                  ? Center(
-                      child: SpinKitDoubleBounce(
-                        color: Colors.red,
-                        size: 100.0,
-                      ),
-                    )
-                  : Container(
-                    height: _screenSize.height * 0.2,
-                    child: GridView.count(
-                        crossAxisCount: 2,
-                        shrinkWrap: true,
-                        children: dataList
-                            .map((dat) => InkWell(
-                                  onTap: () {},
-                                  child: Card(
-                                    elevation: 2.0,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.all(
-                                        Radius.circular(20.0),
+        centerTitle: true,
+      ),
+      body: dataList.length == 0
+          ? Container(
+              child: Center(
+                child: SpinKitDoubleBounce(
+                  color: Colors.red,
+                  size: 100.0,
+                ),
+              ),
+            )
+          : Column(
+              children: <Widget>[
+                Expanded(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ToggleSwitch(
+                        minWidth: 90.0,
+                        cornerRadius: 20,
+                        activeTextColor: Colors.white,
+                        inactiveBgColor: Colors.grey,
+                        inactiveTextColor: Colors.white,
+                        labels: ['Pets', 'Nature'],
+                        activeColors: [Colors.blue, Colors.pink],
+                        onToggle: (index) {
+                          if (index == 0) {
+                            toggle1();
+                          } else {
+                            toggle2();
+                          }
+                        }),
+                  ],
+                )),
+                Expanded(
+                  flex: 2,
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    shrinkWrap: true,
+                    children: dataList
+                        .map((dat) => InkWell(
+                              onTap: () {},
+                              child: Card(
+                                elevation: 2.0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(20.0),
+                                  ),
+                                ),
+                                color: Colors.black12,
+                                child: Column(
+                                  children: <Widget>[
+                                    Expanded(
+                                      flex: 2,
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                            image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: NetworkImage(dat['img']),
+                                        )),
                                       ),
                                     ),
-                                    color: Colors.black12,
-                                    child: Column(
-                                      children: <Widget>[
-                                        Expanded(
-                                            flex: 2,
-                                            child: Container(
-                                              decoration: BoxDecoration(
-                                                  image: DecorationImage(
-                                                fit: BoxFit.cover,
-                                                image: NetworkImage(dat['img']),
-                                              )),
-                                            )),
-                                        SizedBox(
-                                          height: 10.0,
-                                        ),
-                                        Row(
-                                          mainAxisAlignment: MainAxisAlignment.start,
-                                          children: <Widget>[
-                                            Text(
-                                              dat['name'],
-                                              style: TextStyle(
-                                                fontSize: 20.0,
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ],
+                                    SizedBox(
+                                      height: 5.0,
                                     ),
-                                  ),
-                                ))
-                            .toList(),
-                      ),
+                                    Text(
+                                      dat['name'],
+                                      style: TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ))
+                        .toList(),
                   ),
-            ],
-          ),
-        );
+                ),
+              ],
+            ),
+    );
   }
 }
